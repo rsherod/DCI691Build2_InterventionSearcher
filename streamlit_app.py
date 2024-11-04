5903d48b57395baefa86bddea3cc515ffca8ee52
rsherod <rebeccalsherod@gmail.com>
2024-11-03 20:20:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ if "should_generate_response" not in st.session_state:
 # If successful, it shows the image with a caption. If there's an error, it displays an error message instead.
 # You can customize this by changing the image file name and path. Supported image types include .png, .jpg, .jpeg, and .gif.
 # To use a different image, replace 'Build2.png' with your desired image file name (e.g., 'my_custom_image.jpg').
-image_path = 'Tier 2 and Tier 3 Intervention Grid Search.png'
+image_path = 'Tier 2 and Tier 3 Intervention Grid Search.jpg'
 try:
     image = Image.open(image_path)
     st.image(image, caption='Created by Rebecca Sherod (2024)', use_column_width=True)
